57677ba7cc6d71e2e5d30cd781e6858c1d2a77d5
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-02-16 00:03:47 +0000

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
+""" A module for handling squares. """
 
 
-class square():
-    
+class Square:
+    """Represents a rectangle with equal sides."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Initializes a new square. """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """ Computes the area of this square. """
+        return self.width * self.height
 
-    def PerimeterOfMySquare(self):
-        return (self.width * 2) + (self.width * 2)
-    
+    def permiter_of_my_square(self):
+        """ Computes the perimeter of this square. """
+        return (self.width * 2) + (self.height * 2)
+
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
-    
-if __name__ == "__main__":
+        """ Computes the string format of this square. """
+        return "{}/{}".format(self.width, self.height)
 
-    s = square(width=12, height=9)
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PerimeterOfMySquare())
+    print(s.permiter_of_my_square())
